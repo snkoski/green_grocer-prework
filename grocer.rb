6db3b693e00f1b9dc5eefec435b3184b9bd78c83
Shawn Koski <shawnnkoski@gmail.com>
2018-04-10 10:36:26 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -6,7 +6,7 @@ def consolidate_cart(cart)
   cart.each do |items|
     items.each do |name, attributes|
       
-      consolidated[name] = {price: attributes[:price], clearance: attributes[:clearance], count: count.items}
+      consolidated[name] = {price: attributes[:price], clearance: attributes[:clearance], count: cart.count(items)}
       binding.pry
     end
     
