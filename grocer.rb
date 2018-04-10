d2c2e950c915a2b75bed5e478aa92ba796311a9c
Shawn Koski <shawnnkoski@gmail.com>
2018-04-10 10:19:12 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,13 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+
+  count = cart.each_with_object({}) do |key, value|
+     
+     binding.pry
+  end
+  #binding.pry
+ 
 end
 
 def apply_coupons(cart, coupons)
@@ -13,3 +21,11 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+cart = [
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"KALE"    => {:price => 3.0, :clearance => false}}
+]
+
+consolidate_cart(cart)
